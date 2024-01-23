46bc58a7e65ca73bca25ac029261c06c5bd63dbd
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-23 18:10:06 +0400

    Update Foreach3.java

--- Foreach3.java
+++ Foreach3.java
@@ -1,10 +1,17 @@
-public class Foreach3{
-	public static void main(String[] arg){
-		int [] m={5, 8, 9, 12, 13, 16};
-		for(int i:m){
-			if(i%2==0){
-				System.out.println(i);
-			}
-		}
-	}
+public class Foreach3 {
+  public static void main(String[] arg) {
+    int[] m = {
+      5,
+      8,
+      9,
+      12,
+      13,
+      16
+    };
+    for (int i: m) {
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+  }
 }
